50293a7f33740ab00c8403b1e9319593ae327974
Vladimir <mail@myexample.com>
2022-03-03 20:50:11 +0300

    Задача x

---
+++ lesson2_2.cs
@@ -0,0 +1,22 @@
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 6:
+    {
+        Console.WriteLine("Да");
+        break;
+    }
+        case 7:
+    {
+        Console.WriteLine("Да");
+        break;
+    }
+    default: 
+    {
+        Console.WriteLine("нет");
+        break;
+    }
+}
+  
