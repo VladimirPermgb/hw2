e328b14dd73b6486891d78ae9f082f5b578cb603
Vladimir <mail@myexample.com>
2022-03-03 20:43:07 +0300

    Work done

--- lesson2.cs
+++ lesson2.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 6:
+    {
+        Console.WriteLine("Да");
+        break;
+    }
+        case 7:
+    {
+        Console.WriteLine("Да");
+        break;
+    }
+    default: 
+    {
+        Console.WriteLine("нет");
+        break;
+    }
+}
