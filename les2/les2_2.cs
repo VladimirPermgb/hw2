618419bd1dfebcb9db0d1886637e37aff4b36743
Vladimir <mail@myexample.com>
2022-03-06 18:50:57 +0300

    Задача 10

--- les2/les2_2.cs
+++ les2/les2_2.cs
@@ -1,3 +1,8 @@
 ﻿Console.WriteLine("Введите трёхзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
+int n1 = number / 100;
+int n2 = number - n1 * 100;
+int n3 = n2 / 10;
+
+Console.WriteLine(n3);
