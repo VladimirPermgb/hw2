7ebab15e799250ec34433fe9ea981fe6db9ce362
VladimirPermgb <100301190+VladimirPermgb@users.noreply.github.com>
2022-04-18 07:04:53 +0300

    Урок 10

--- lesson2.cs
+++ lesson2.cs
@@ -1,146 +1,66 @@
-// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Задача 3: Заданы 2 массива: info и data. В массиве data хранятся двоичные представления 
+// нескольких чисел (без разделителя). В массиве info хранится информация о количестве бит, которые
+// занимают числа из массива data. Напишите программу, которая составит массив десятичных представлений
+// чисел массива data с учётом информации из массива info.
+// Пример:
+// входные данные:
+// data = {0, 1, 1, 1, 1, 0, 0, 0, 1}
+// info = {2, 3, 3, 1}
+// выходные данные:
+// 1, 7, 0, 1
 
-
-int [,] CreateMatrix (int row, int col)
+int BinaryToDecimal(int[] array, int sum = 0)
 {
-    int [,] array = new int [row, col];
-    for (int i = 0; i < row; i++)
-    {
-        for (int j =0; j < col; j++)
-        {
-            array [i, j] = new Random().Next (10, 99);
-        }
-    }
-    return array;
+    Array.Reverse(array);
+    for (int i = 0; i < array.Length; i++)
+        sum += array[i] * (int)Math.Pow(2,i);
+
+    return sum;
 }
 
-void PrintMatrix(int[,] array)
+int[] PrintDataByInfo(int[] data, int[] info)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int sum = 0;
+    for (int i = 0; i < info.Length; i++)
+        sum += info[i];
+
+    int[] dec = new int[info.Length];
+
+    if (sum == data.Length)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        Console.WriteLine("Input: ");
+        Console.Write("data = {");
+        for (int j = 0; j < data.Length-1; j++)
+            Console.Write($"{data[j]}, ");
+        Console.WriteLine($"{data[data.Length-1]}" + "}");
+
+        Console.Write("info = {");
+        for (int j = 0; j < info.Length-1; j++)
+            Console.Write($"{info[j]}, ");
+        Console.WriteLine($"{info[info.Length-1]}" + "}");
+
+        for (int i = info.Length - 1; i >= 0; i--)
         {
-            Console.Write($"{array[i, j]} ");
+            dec[i] = BinaryToDecimal(data.ToList().GetRange(data.Length - info[i], info[i]).ToArray());
+            data = data.ToList().GetRange(0, data.Length-info[i]).ToArray();
         }
-        Console.WriteLine();
     }
+    else Console.WriteLine("info и data не согласованы");
+
+    return dec;
 }
 
-int [,] RawSort (int [,] array)
+void PrintArray(int[] array)
 {
-    for (int i=0; i<array.GetLength(0); i++)
-    {
-        for (int j=0; j<array.GetLength(1)-1; j++)
-        {
-            for (int k= j+1; k<array.GetLength(1); k++) 
-            {
-                if (array [i,j] > array [i, k]) 
-                {
-                    int temp = array [i,j];
-                    array [i,j] = array [i,k];
-                    array [i,k] = temp;
-                }
-            }
-        }
-    }
-    return array;
-}
-
-
-int row = 3;
-int col = 3;
-int [,] matrix = CreateMatrix (row,col);
-PrintMatrix (matrix);
-Console.WriteLine ();
-PrintMatrix (RawSort (matrix));
-
-// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
-
-int MinSumRaw (int [,] matrix)
-{
-    int minSum=0;
-    int minIndex=0;
-    for (int i = 0; i<matrix.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j<matrix.GetLength(1); j++)
-        {
-            sum = sum + matrix[i,j];
-        }
-        Console.WriteLine (sum);
-        if (minSum>sum || i==0)
-        {
-            minSum=sum;
-            minIndex=i;
-        }
-    }
-    Console.WriteLine ();
-    return minIndex;
-}
-int row = 3;
-int col = 4;
-int [,] matrix = CreateMatrix (row,col);
-PrintMatrix (matrix);
-Console.WriteLine ();
-Console.WriteLine (MinSumRaw (matrix));
-
-// Задача 3: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-
-
-int[,,] CreateMass(int row, int col, int layer)
-{
-    int[,,] array = new int[row, col, layer];
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            for (int k = 0; k < layer; k++)
-            {
-                bool flag = true;
-                while (flag)
-                {
-                    flag = false;
-                    int kandidat = new Random().Next(10, 99);
-                    for (int i1 = 0; i1 < row; i1++)
-                    {
-                        for (int j1 = 0; j1 < col; j1++)
-                        {
-                            for (int k1 = 0; k1 < layer; k1++)
-                            {
-                                if (array[i1, j1, k1] == kandidat) flag = true;
-
-                            }
-                        }
-                    }
-                    if (!flag) array[i, j, k] = kandidat;
-
-                }
-
-            }
-        }
-    }
-    return array;
-}
-
-void PrintMass(int[,,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
-            }
-        }
-        Console.WriteLine();
-    }
+    Console.WriteLine("Output: ");
+    for (int i = 0; i < array.Length-1; i++)
+        Console.Write($"{array[i]}, ");
+    Console.WriteLine($"{array[array.Length-1]}");
 }
 
 
-int row = 3;
-int col = 3;
-int layer = 3;
-int [,,] array123 = CreateMass (row,col, layer);
-PrintMass (array123);
+Console.Clear();
+
+int[] data = {0, 1, 1, 1, 1, 0, 0, 0, 1, 1},
+      info = {2, 3, 3, 2};     
+PrintArray(PrintDataByInfo(data, info));
